c79c1e6ac2da2a52ca09d422229ebad13cfc3d7f
Josh <josh@opaliq.com>
2023-07-05 15:59:18 -0400

    original run command

--- run_tree.sh
+++ run_tree.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Navigate to the script's directory
-cd "$(dirname "$0")"
-
+### FOR DEVELOPER ###
 # Run the application
-python3 combine_code.py
+python3 ~/_DEV/_INTERNAL/tree_cli/combine_code.py
+
+
+### FOR USER ###
+# # Navigate to the script's directory
+# cd "$(dirname "$0")"
+
+# # Run the application
+# python3 combine_code.py
