db4e5f482be1771cfcc9eced2d8030d8944a54fe
Josh <josh@opaliq.com>
2023-09-15 17:27:29 -0400

    added formatting

--- combine_code.py
+++ combine_code.py
@@ -68,7 +68,7 @@ def combine_files(root_dir, output_dir="./", ignore_file=".gitignore", **kwargs)
                 outfile.write(INSTRUCTIONS.strip() + "\n\n")
                 outfile.write(f'# {"=" * 80}\n\n')
 
-            outfile.write(f'# {"=" * 80}\n\n')
+            outfile.write(f'# {"=" * 80}\n')
             outfile.write("# Project Structure:\n")
             outfile.write(f'# {"=" * 80}\n\n')
             formatted_structure = "\n".join(directory_tree._generator._tree)
